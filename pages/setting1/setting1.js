75d8ce998061a27d85f547bdc09326b08943514c
xuxu <chenxu4012@foxmail.com>
2020-05-13 15:57:26 +0800

    完成页面form

--- pages/setting1/setting1.js
+++ pages/setting1/setting1.js
@@ -1,66 +1,41 @@
 // pages/setting1/setting1.js
+import event from '../../utils/event'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    language: '',
+    toptipShow: false,
+    topTipMsg: '你好啊'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setLanguage();	// (1)
+    event.on("languageChanged", this, this.setLanguage); // (2)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  setLanguage () {
+    this.setData({
+      language: wx.T.getLanguage()
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // showToptip
+  showTopTip (event) {
+    const page = event.currentTarget.dataset.page
+    const key = event.currentTarget.dataset.key
+    console.log(page, key)
+    console.log(this.data.language[page][key])
+    this.setData({
+      toptipShow: true,
+      // toptipMessage: this.data.language[page][key]
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 退出断开连接
+  logOut () {
+    wx.redirectTo({ url: '../index/index' });
   }
 })
--- utils/locales.js
+++ utils/locales.js
@@ -1,40 +1,12 @@
 export default {
   'zh_cn': {
     'language': '简体中文',
-    'login': '登录',
-    'logout': '登出',
-    'authorization': '授权',
-    'pay': '支付',
-    'changeLanguage': '切换语言',
-    'search': '搜索',
-    'investment': '投资金额',
-    'invest_industry': '投资行业',
-    'invest_stage': '投资阶段',
-    'address': '地址',
-    'createdate': '成立时间',
-    'clubregisternum': '协会人数',
-    'classification': '分类',
-    'area': '区域',
-    'essential_information': '基本信息',
-    'association_name': '社团名称',
-    'association_address': '社团地址',
-    'contacts': '联 系 人',
-    'phone': '联系电话',
-
+    'setting1': {
+      'topTip1': '设置开闸后，无人通行，闸机自动关闸的时间，默认为 5秒。数据可取范围为3~250'
+    }
   },
 
   'en': {
-    "language": "Language",
-    "languageName": "English",
-    "login": "Log in",
-    "logout": "Log out",
-    "changed": "Changed successfully",
-    "loginSuccessed": "Logged in", "loginFailed": "Logged in failed",
-    "logining": "Logging in",
-    "requestError": "Request error",
-    "personalInfo": "Personal information",
-    "settings": "Settings",
-    "checkIn": "Check in",
-    "checkOut": "Check out"
+    "language": "Language"
   }
 }
